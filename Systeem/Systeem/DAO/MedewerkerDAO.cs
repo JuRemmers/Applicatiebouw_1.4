ce4c87563b9d75d7193252cd566dabb76ebaa31e
Donna0999 <donna@vanderbent.net>
2017-06-07 14:09:14 +0200

    Medewerker + medewerkerDAO

--- Systeem/Systeem/DAO/MedewerkerDAO.cs
+++ Systeem/Systeem/DAO/MedewerkerDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using Systeem.Model;
 
 namespace Systeem.DAO
 {
@@ -16,6 +17,52 @@ namespace Systeem.DAO
         {
             dbconn = new DBconnection();
             conn = dbconn.GetConnection();
+        }
+
+        public Medewerker ReadMedewerker(SqlDataReader reader)
+        {
+            try
+            {
+                int id = (int)reader["ID"];
+                string voornaam = (string)reader["Voornaam"];
+                string achternaam = (string)reader["Achternaam"];
+                string functie = (string)reader["Functie"];
+                string wachtwoord = (string)reader["Wachtwoord"];
+
+                Functie fun;
+                switch (functie)
+                {
+                    case "Bar":
+                        { fun = Functie.Bar; break; }
+                    case "Bediening":
+                        { fun = Functie.Bediening; break; }
+                    case "Keuken":
+                        { fun = Functie.Keuken; break; }
+                    case "Beheer":
+                        { fun = Functie.Beheer; break; }
+                    default:
+                        { fun = Functie.Bediening; break; }
+                }
+                return new Medewerker(id, voornaam, achternaam, fun, wachtwoord);
+            }
+            catch
+            { return null; }
+        }
+
+        public Medewerker GetForID(int id)
+        {
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Medewerker WHERE ID=" + id, conn);
+            SqlDataReader reader = command.ExecuteReader();
+
+            reader.Read();
+            Medewerker medewerker = ReadMedewerker(reader);
+
+            reader.Close();
+            conn.Close();
+
+            return medewerker;
         }
     }
 }
--- Systeem/Systeem/Model/Medewerker.cs
+++ Systeem/Systeem/Model/Medewerker.cs
@@ -13,5 +13,16 @@ namespace Systeem.Model
         private string achternaam;
         private Functie functie;
         private string wachtwoord;
+        
+        public Medewerker(int id, string name, string surname, Functie function, string password)
+        {
+            this.ID = id;
+            this.voornaam = name;
+            this.achternaam = surname;
+            this.functie = function;
+            this.wachtwoord = password;
+        }
     }
+
+    
 }
