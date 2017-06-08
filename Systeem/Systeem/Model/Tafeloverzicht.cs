bda6f151bbb8ea6ad493d6c31a648a4a8f3c5446
Kayleigh <kayleigh.vossen@hotmail.com>
2017-06-08 13:22:32 +0200

    tafeloverzicht

---
+++ Systeem/Systeem/Model/Tafeloverzicht.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Systeem.Model
+{
+    class Tafeloverzicht
+    {
+        public int tafelId;
+        public List<Bestelling> bestellingen;
+
+        public Tafeloverzicht()
+        {
+
+        }
+    }
+}
