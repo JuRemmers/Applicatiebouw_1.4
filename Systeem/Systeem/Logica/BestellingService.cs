ddf4acb0d16e534471369c7d73bab9d7de4b1022
JuRemmers <jul.remmers@gmail.com>
2017-06-08 16:18:11 +0200

    fuck jou met je uncommited changes

--- Systeem/Systeem/Logica/BestellingService.cs
+++ Systeem/Systeem/Logica/BestellingService.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Systeem.Model;
+using Systeem.DAO;
 
 namespace Systeem.Logica
 {
     class BestellingService
     {
-        //public List<Bestelling> GetAllForKaart(string kaart)
-        //{
-        //}
+        public List<Bestelling> GetAllForBestelling(string locatieid)
+        {
+            BestelItemDAO BestelDAL = new BestelItemDAO();
+            List<Bestelling> Bestellingen = BestelDAL.GetAllBestellingen(locatieid);
+            return Bestellingen;
+        }
     }
 }
