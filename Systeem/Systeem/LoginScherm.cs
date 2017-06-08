f2907fc27c61c9ec7a8dd4f613579dc3b289b0f9
Donna0999 <donna@vanderbent.net>
2017-06-08 13:38:01 +0200

    llogingscherm

--- Systeem/Systeem/LoginScherm.cs
+++ Systeem/Systeem/LoginScherm.cs
@@ -25,34 +25,38 @@ namespace Systeem
 
         private void b_login_enter_Click(object sender, EventArgs e)
         {
+            int werknemer_id=0;
+            string wachtwoord=" ";
+
             try
             {
-                int werknemer_id;
-                string wachtwoord;
-
-
                 werknemer_id = Int32.Parse(tb_werknemer_id.Text);
                 wachtwoord = tb_wacthwoord.Text.ToString();
-
-                Loginservice login = new Loginservice();
-                bool check = login.logincheck(werknemer_id, wachtwoord);
-                if (check)
-                {
-                    this.Hide();
-                    var TafelOverzicht = new TafelOverzicht();
-                    TafelOverzicht.ShowDialog();
-                }
-                else
-                {
-                    MessageBox.Show("Incorect wachtwoord");
-                }
             }
             catch
             {
                 MessageBox.Show("Verkeerde invoer, onthoud dat Werknemer ID een getal moet zijn");
             }
 
+            Loginservice login = new Loginservice();
+            bool check = login.logincheck(werknemer_id, wachtwoord);
+            if (check)
+            {
+                this.Hide();
+                var TafelOverzicht = new TafelOverzicht();
+                TafelOverzicht.ShowDialog();
+                this.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Incorect wachtwoord");
+            }
+
+
             
+
+
         }
 
         private void tb_wacthwoord_TextChanged(object sender, EventArgs e)
