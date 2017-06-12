c5210948073f380941c7b01da7dbfd19bc936cba
Jesse <jesse@LAPTOP-5HJT7SQ5>
2017-06-12 16:38:01 +0200

    Commits op loginstuk

--- Systeem/Systeem/Logica/Loginservice.cs
+++ Systeem/Systeem/Logica/Loginservice.cs
@@ -12,19 +12,19 @@ namespace Systeem.Logica
     {
         public bool logincheck(int werknemer_id, string wachtwoord)
         {
-            MedewerkerDAO medewerkerDAL = new MedewerkerDAO();
+            MedewerkerDAO medewerkerDAL = new MedewerkerDAO(); // Nieuwe instantie van medewerker
 
-            Medewerker medewerker = medewerkerDAL.GetForID(werknemer_id);
+            Medewerker medewerker = medewerkerDAL.GetForID(werknemer_id); // Roept methode GetForID aan uit MedewerkerDAO in een nieuwe instantie van medewerker (Model-laag)
 
             bool result;
 
-            if (medewerker != null)
+            if (medewerker != null) 
             {
-                result = medewerker.CheckWachtwoord(wachtwoord);
-                return result;
+                result = medewerker.CheckWachtwoord(wachtwoord); // Roept methode CheckWachtwoord aan uit Medewerker (model-laag)
+                return result; // Returned de bool als invoer medewerker niet herkent wordt in database
             }
 
-            else return false;           
+            else return false; // Als medewerker niet herkent wordt, wordt ook niks gereturned            
         }
     }
 }
